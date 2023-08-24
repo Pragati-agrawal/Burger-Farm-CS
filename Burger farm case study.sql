09865ffd9d33360bf2da92d83dd07114166b4646
Pragati-agrawal <76447782+Pragati-agrawal@users.noreply.github.com>
2023-08-25 01:53:03 +0530

    Update Burger farm case study.sql

--- Burger farm case study.sql
+++ Burger farm case study.sql
@@ -23,29 +23,15 @@ Select runner_id, count(distinct(order_id)) as "successful orders" from runner_o
   group by c.customer_id, n.burger_name
   order by c.customer_id, n.burger_name asc;
 
-##Q6 For each customer, how many delivered burgers had atleast 1 change and how many had no changes?
-Select c.customer_id, sum(case when c.exclusions<> " " or c.extras<>' '
-then 1 
-else 0
-end) as "atleast 1 change", 
-sum(case when c.exclusions =' ' or c.extras = ' '
-then 1 
-else 0
-end) as "no change"
-from customer_orders as c join runner_orders as r 
-on c.order_id = r.order_id where r.distance!=0
-group by customer_id
-order by c.customer_id;
-
-##Q7 What was the total volume of burgers ordered for each hour of the day?
+##Q6 What was the total volume of burgers ordered for each hour of the day?
 select extract(hour from order_time) as hour_of_day, count(order_id) as burger_count from customer_orders
 group by extract(hour from order_time);
 
-##Q8 how many runners signed up for each 1 week period?
+##Q7 how many runners signed up for each 1 week period?
 select extract(week from registration_date) as registration_week, count(runner_id) as runner_signup from burger_runner
 group by extract(week from registration_date);
 
-##Q9  What was the average distance travelled for each customer?
+##Q8  What was the average distance travelled for each customer?
 Select c.customer_id, avg(r.distance) as "avg distance"
 from customer_orders as c join runner_orders as r
 on c.order_id = r.order_id where r.distance!=0
